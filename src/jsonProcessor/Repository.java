325366248d30c7c50df9cb045cda68ea091aa9b4
albertusdj <albertusdjauharidjohan@gmail.com>
2017-06-06 22:41:42 +0700

    Menambah kelas Repository
    Kelas yang mewakili repository user

---
+++ src/jsonProcessor/Repository.java
@@ -0,0 +1,11 @@
+package jsonProcessor;
+
+public class Repository {
+	String nama;
+	String deskripsi;
+	String url;
+	
+	public String toString(){
+		return nama + ' ' + deskripsi + ' ' + url + '\n';
+	}
+}
