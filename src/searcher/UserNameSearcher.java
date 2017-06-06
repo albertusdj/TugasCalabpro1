f06396cf4d847742116dd7e304297951107ba6d6
albertusdj <albertusdjauharidjohan@gmail.com>
2017-06-06 22:50:30 +0700

    Menyederhanakan kelas UserNameSearcher
    Penyederhanaan dengan menggunakan kelas Connector

--- src/searcher/UserNameSearcher.java
+++ src/searcher/UserNameSearcher.java
@@ -1,32 +1,16 @@
 package searcher;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class UserNameSearcher{
 	private String dataUser;
 	
 	public UserNameSearcher(String userName) throws MalformedURLException, IOException{
-	    String url = "https://api.github.com/search/users?q="+userName+"in%3Alogin&type=Users";
-	    URL web = new URL(url);
-	        
-	    URLConnection connection = web.openConnection();
-	    BufferedReader in;
-	    in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-	    String inputLine;
-	        
-	    dataUser = "";
+	    String url = "https://api.github.com/search/users?q="+userName+"+in%3Alogin&type=Users";
 	    
-	    while((inputLine = in.readLine())!=null){
-	        byte data[] = inputLine.getBytes();
-	        dataUser += new String(data);
-	    }
-	        
-	    in.close();
+	    Connector connector = new Connector(url);
+	    dataUser = connector.getString();
 	}
 	
 	public String getString(){
