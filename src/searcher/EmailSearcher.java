e94c7259ceaf101cce4019be7b23ad77957ad4db
albertusdj <albertusdjauharidjohan@gmail.com>
2017-06-06 22:45:42 +0700

    Menyederhanakan kelas EmailSearcher
    Penyederhanaan dilakukan dengan kelas Connector

--- src/searcher/EmailSearcher.java
+++ src/searcher/EmailSearcher.java
@@ -1,35 +1,20 @@
 package searcher;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class EmailSearcher {
 	private String dataUser;
 	
 	public EmailSearcher(String email) throws MalformedURLException, IOException{
-	    String url = "https://api.github.com/search/users?q="+email+"in%3Aemail&type=Users";
-	    URL web = new URL(url);
-	        
-	    URLConnection connection = web.openConnection();
-	    BufferedReader in;
-	    in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-	    String inputLine;
+	    String url = "https://api.github.com/search/users?q="+email+"+in%3Aemail&type=Users";
 	    
-	    dataUser = "";
-	    
-	    while((inputLine = in.readLine())!=null){
-	        byte data[] = inputLine.getBytes();
-	        dataUser += new String(data);
-	    }
-	        
-	    in.close();
+	    Connector connector = new Connector(url);
+	    dataUser = connector.getString();
 	}
 	
 	public String getString(){
 		return dataUser;
 	}
 }
+
